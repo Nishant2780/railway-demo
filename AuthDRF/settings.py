30e5c163000ec569da57d525f53bb94972622f04
Nishant Patel <99729230+Nishant2780@users.noreply.github.com>
2023-02-08 12:51:51 +0530

    allow host

--- AuthDRF/settings.py
+++ AuthDRF/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-008p5mqrqe2dgivl$fn=r7j!kgh=vrnrxh!wc4@pxvs0^j#f7b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
